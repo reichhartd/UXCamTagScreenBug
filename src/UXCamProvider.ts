3e710d914f43ad112e943ef3589407f0df9be5c1
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-09-27 09:22:53 +0200

    Prettier config

--- src/UXCamProvider.ts
+++ src/UXCamProvider.ts
@@ -1,13 +1,13 @@
 import RNUxcam from 'react-native-ux-cam';
 
 RNUxcam.optIntoSchematicRecordings(); // Add this line to enable iOS screen recordings
-RNUxcam.setAutomaticScreenNameTagging(false)
+RNUxcam.setAutomaticScreenNameTagging(false);
 const configuration = {
-  userAppKey: 'YOUR API KEY',
-  /*
+    userAppKey: 'YOUR API KEY',
+    /*
       disable advanced gestures if you're having issues with
       swipe gestures and touches during app interaction
   */
-  // enableAdvancedGestureRecognition: false
-}
+    // enableAdvancedGestureRecognition: false
+};
 RNUxcam.startWithConfiguration(configuration);
